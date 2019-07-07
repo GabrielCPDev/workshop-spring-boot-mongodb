93a2525a9e0ff3730c6467db34ec639bc6bf5177
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2019-07-07 17:02:44 -0300

    consulta com varios criterios

--- src/main/java/com/gabrielc/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gabrielc/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.gabrielc.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,6 +16,10 @@ public interface PostRepository extends MongoRepository<User, String> {
 	List<Post> searchTitle(String text);
 	
 	List <Post> findByTitleContaninigIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
 	void saveAll(List<Post> asList);
 
 }
